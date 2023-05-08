fff8eac2490a182d7c1d59e51506b8b0804d4eaf
이민희 <mhl98@dgu.ac.kr>
2023-05-08 23:44:00 +0900

    feat: 로고 이미지 추가, 헤더에 글 작성 버튼 추가

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
 import sidebarIcon from '../../assets/icons/sidebar.svg';
+import penIcon from '../../assets/icons/pen.svg';
+import logoIcon from '../../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <MainHeader>
-      <h1>데이트 버즈</h1>
-      <img src={sidebarIcon} alt="사이드바 열기 아이콘" />
+      <LogoImage src={logoIcon} />
+      <IconBox>
+        <img src={penIcon} alt="글 작성하기 아이콘" onClick={() => navigate(`/selectTags`)} />
+        <img src={sidebarIcon} alt="사이드바 열기 아이콘" />
+      </IconBox>
     </MainHeader>
   );
 };
@@ -20,4 +27,11 @@ const MainHeader = styled.header`
 
   width: 100%;
   height: 6rem;
+`;
+const LogoImage = styled.img`
+  width: 15rem;
+`;
+const IconBox = styled.div`
+  display: flex;
+  gap: 1rem;
 `;
