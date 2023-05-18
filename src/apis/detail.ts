ebcd8195e41bfcd3add53a0e35135390437dcf7f
wooyoung0830 <thddndud0830@gmail.com>
2023-05-18 22:50:45 +0900

    feat : 게시글 삭제 API

--- src/apis/detail.ts
+++ src/apis/detail.ts
@@ -1,6 +1,11 @@
-import { GET } from 'apis/api';
+import { GET, DELETE } from 'apis/api';
 
 export const getCommunityDetail = async (postId: number) => {
   const { data } = await GET(`/community/${postId}`);
+  return data;
+};
+
+export const deleteCommunityDetail = async (postId: number, token: string) => {
+  const { data } = await DELETE(`/community/${postId}`, token);
   return data;
 };
