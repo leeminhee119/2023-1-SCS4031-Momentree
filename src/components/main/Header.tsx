dec810021a37768d04ae19ba8e03a6736e6c5537
wooyoung0830 <thddndud0830@gmail.com>
2023-05-19 16:40:13 +0900

    feat : 메인페이지와 로그인 페이지 연결

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,19 +1,16 @@
 import styled from 'styled-components';
 import sidebarIcon from '../../assets/icons/sidebar.svg';
-import penIcon from '../../assets/icons/pen.svg';
 import logoIcon from '../../assets/logo.png';
-import { useNavigate } from 'react-router-dom';
+
 import Nav from 'pages/Nav';
 import { useState } from 'react';
 
 const Header = () => {
-  const navigate = useNavigate();
   const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
   return (
     <MainHeader>
       <LogoImage src={logoIcon} />
       <IconBox>
-        <img src={penIcon} alt="글 작성하기 아이콘" onClick={() => navigate(`/selectTags`)} />
         <img
           src={sidebarIcon}
           alt="네브바 열기 아이콘"
