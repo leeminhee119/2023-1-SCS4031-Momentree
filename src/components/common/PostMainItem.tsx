5503c425e2b00929cb0d8e0868d1d19d9c9f9064
wooyoung0830 <thddndud0830@gmail.com>
2023-05-21 12:03:59 +0900

    feat : 로그인 안했을때 북마크, 좋아요 누르면 로그인페이지로 이동

--- src/components/common/PostMainItem.tsx
+++ src/components/common/PostMainItem.tsx
@@ -32,7 +32,7 @@ const PostMainItem = ({
       {bookMarkStatus ? (
         <BookmarkIcon
           onClick={() => {
-            postBookmarkMutation.mutate();
+            cookies?.user?.userToken ? postBookmarkMutation.mutate() : navigate('/login');
           }}
           src={clickbookmarkIcon}
           alt="북마크 한 아이콘"
@@ -41,7 +41,7 @@ const PostMainItem = ({
         <BookmarkIcon
           src={unclickbookmarkIcon}
           onClick={() => {
-            postBookmarkMutation.mutate();
+            cookies?.user?.userToken ? postBookmarkMutation.mutate() : navigate('/login');
           }}
           alt="북마크 하지 않은 아이콘"
         />
--- src/hooks/queries/useLogin.ts
+++ src/hooks/queries/useLogin.ts
@@ -1,4 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { postLogin } from 'apis/login';
 import { useCookies } from 'react-cookie';
 
@@ -7,10 +7,12 @@ interface LoginResult {
 }
 export const useLoginMutation = (userName: string, password: string) => {
   const [cookies, setCookie] = useCookies(['user']);
+  const queryClient = useQueryClient();
 
   return useMutation(() => postLogin({ userName: userName, password: password }), {
     onSuccess: (data: LoginResult) => {
       setCookie('user', { userToken: `Bearer ${data.result.token}`, userName: userName }); // 쿠키에 토큰 저장
+      queryClient.invalidateQueries(['getCommunity']);
       return cookies;
     },
   });
