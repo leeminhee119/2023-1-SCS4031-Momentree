e97574a8c573c699f53556bb916e623a3ebe403a
banban555 <ban_ban_55@naver.com>
2023-05-24 13:01:51 +0900

    feature:해시태그 검색 뷰 수정

--- src/pages/PostsByTagPage.tsx
+++ src/pages/PostsByTagPage.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import leftIcon from '../assets/icons/left.svg';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { CommunityData } from '../types/communityData';
 import PostItem from 'components/common/PostMainItem';
+import styled from 'styled-components';
 
 const PostsByTagPage = () => {
+  const navigate = useNavigate();
   const { tag } = useParams<{ tag?: string }>();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [posts, setPosts] = useState<CommunityData[]>([]);
-  console.log('tag: ', tag); // tag 확인
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -28,20 +30,55 @@ const PostsByTagPage = () => {
 
   return (
     <div>
-      {posts.map((data) => (
-        <PostItem
-          recordedId={data?.recordedId}
-          title={data?.title}
-          bookMarkStatus={data?.bookMarkStatus}
-          likeCnt={data?.likeCnt}
-          bookmarkCnt={data?.bookMarkCnt}
-          vibeTag={data?.vibeTags}
-          activityTag={data?.activityTags}
-          place={data?.recordedPlaces}
-          key={data?.recordedId}></PostItem>
-      ))}
+      <PostByPageContainer>
+        <PostByPageHeader>
+          <Icon
+            src={leftIcon}
+            alt="뒤로가기 아이콘"
+            onClick={() => {
+              navigate('/');
+              window.location.reload();
+            }}
+          />
+          <h1>{tag} 해시태그가 포함된 게시물</h1>
+        </PostByPageHeader>
+        {posts.map((data) => (
+          <PostItem
+            recordedId={data?.recordedId}
+            title={data?.title}
+            bookMarkStatus={data?.bookMarkStatus}
+            likeCnt={data?.likeCnt}
+            bookmarkCnt={data?.bookMarkCnt}
+            vibeTag={data?.vibeTags}
+            activityTag={data?.activityTags}
+            place={data?.recordedPlaces}
+            key={data?.recordedId}></PostItem>
+        ))}
+      </PostByPageContainer>
     </div>
   );
 };
+
+const PostByPageContainer = styled.section`
+  h1 {
+    color: ${({ theme }) => theme.colors.gray900};
+    ${({ theme }) => theme.fonts.heading2};
+  }
+`;
+
+const PostByPageHeader = styled.article`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 2.2rem;
+`;
+
+const Icon = styled.img`
+  width: 19.67px;
+  height: 16.67px;
+  cursor: pointer;
+`;
 
 export default PostsByTagPage;
