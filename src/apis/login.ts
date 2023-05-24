ad998217ad63790757b6fafa6b702d549a883fe0
banban555 <ban_ban_55@naver.com>
2023-05-24 13:37:02 +0900

    feat: signup.ts 서버 요청 수정

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -1,13 +1,9 @@
 import { POST } from 'apis/api';
-
 interface IBody {
   userName: string;
-  email: string;
   password: string;
-  nickname: string;
 }
-
-export const postSignup = async (body: IBody) => {
-  const { data } = await POST('/join', body);
+export const postLogin = async (body: IBody) => {
+  const { data } = await POST('/login', body);
   return data;
 };
