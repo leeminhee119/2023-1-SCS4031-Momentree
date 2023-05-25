72d1d94822f56fff2874bfdf39a25b30cfdbfe68
banban555 <ban_ban_55@naver.com>
2023-05-25 10:40:02 +0900

    getHashTagPost api 추가

--- src/hooks/queries/useCommunity.ts
+++ src/hooks/queries/useCommunity.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { getCommunity } from 'apis/main';
+import { getHashtagPost } from 'apis/hashtagPost';
 
 export const useCommunityQuery = (page: number, size: number, token?: string) => {
   const data = useQuery(['getCommunity', page], () => getCommunity(page, size, token));
