f00c595bf305ad069db47eb6f42f599dab397bbe
banban555 <ban_ban_55@naver.com>
2023-05-29 23:51:22 +0900

    feat:추천서비스 예외화면 구현

--- src/components/main/Recommendation.tsx
+++ src/components/main/Recommendation.tsx
@@ -1,13 +1,54 @@
 import styled from 'styled-components';
 import RecommendationItem from './RecommendationItem';
 import { useNavigate } from 'react-router-dom';
-import { useRecommendQuery } from '../../hooks/queries/useRecommend';
+import heartIcon from '../../assets/icons/loadheart.svg';
 import { CommunityData } from 'types/communityData';
 import { useCookies } from 'react-cookie';
+import { useQuery } from '@tanstack/react-query';
+import { getRecommendList } from 'apis/main';
+import theme from '../../styles/theme';
 
 const RecommendationContainer = styled.section`
   display: flex;
   flex-direction: column;
+`;
+
+const LodingContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 16.2rem;
+  align-item: center;
+`;
+
+const LodingContainerTitle = styled.h1`
+  margin-top: 2rem;
+  margin-bottom: 1.6rem;
+  text-align: left;
+  ${({ theme }) => theme.fonts.body1};
+  color: ${({ theme }) => theme.colors.gray900};
+`;
+
+const LodingTextContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 6.2rem;
+`;
+
+const LodingText = styled.p`
+  font: ${theme.fonts.caption4};
+  color: ${theme.colors.gray600};
+  text-align: center;
+  margin: 0.3rem 0rem;
+`;
+
+const HeartIcon = styled.img`
+  color: ${theme.colors.gray200};
+  width: 2.2rem;
+  height: 2.2rem;
+  margin: 0.8rem;
 `;
 
 const RecommendationContainerTitle = styled.h1`
@@ -29,9 +70,38 @@ const Recommendation = () => {
   const [cookies] = useCookies(['user']);
   const navigate = useNavigate();
 
-  const { data } = useRecommendQuery(cookies?.user?.userToken);
-  console.log(data?.result);
+  const token = cookies?.user?.userToken;
+
+  const { data, isLoading } = useQuery(['getRecommendList', token], () => getRecommendList(token), {
+    enabled: !!token, // token이 존재할 때만 쿼리를 실행합니다.
+  });
+
   const content = data?.result || [];
+
+  if (!token || isLoading) {
+    return (
+      <LodingContainer>
+        <LodingContainerTitle>당신을 위한 추천 코스</LodingContainerTitle>
+        <LodingTextContainer>
+          <HeartIcon src={heartIcon} alt="하트 아티콘"></HeartIcon>
+          <LodingText>로그인 후 이용할 수 있는 서비스입니다.</LodingText>
+        </LodingTextContainer>
+      </LodingContainer>
+    );
+  }
+
+  if (content.length === 0) {
+    return (
+      <LodingContainer>
+        <LodingTextContainer>
+          <LodingContainerTitle>당신을 위한 추천 코스</LodingContainerTitle>
+          <HeartIcon src={heartIcon} alt="하트 아티콘"></HeartIcon>
+          <LodingText>좋아요 및 북마크가 비어있네요!</LodingText>
+          <LodingText>관심있는 게시글에 좋아요와 북마크를 표시해보세요.</LodingText>
+        </LodingTextContainer>
+      </LodingContainer>
+    );
+  }
 
   return (
     <RecommendationContainer>
@@ -43,7 +113,8 @@ const Recommendation = () => {
               onClick={() => {
                 navigate(`/post/${data.recordedId}`);
                 window.location.reload();
-              }}>
+              }}
+              key={index}>
               <RecommendationItem
                 recordedId={data?.recordedId}
                 title={data.title}
@@ -51,7 +122,7 @@ const Recommendation = () => {
                 likeCnt={data.likeCnt}
                 bookmarkCnt={data.bookMarkCnt}
                 place={data.recordedPlaces}
-                key={index}></RecommendationItem>
+              />
             </div>
           );
         })}
--- src/hooks/queries/useRecommend.ts
+++
@@ -1,7 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getRecommendList } from 'apis/main';
-
-export const useRecommendQuery = (token: string) => {
-  const response = useQuery([getRecommendList], () => getRecommendList(token));
-  return response;
-};
